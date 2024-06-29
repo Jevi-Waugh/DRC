917d1593fc565d332dafa9fa7a2319153f197463
Jevi Waugh <codeprogramming99@gmail.com>
2024-06-29 19:30:32 +1000

    motor functions defined

---
+++ motor.py
@@ -0,0 +1,18 @@
+class motor:
+    def __init__(self, speed=0):
+        self.speed = speed
+        
+    def move_foward(self, seconds=None): # Not implemented -> Last minute
+        pass
+    
+    def turn_right(self, degree): # Not implemented -> Last minute
+        pass
+    
+    def turn_left(self, degree): # Not implemented -> Last minute
+        pass
+    
+    def reverse(self): # Not implemented -> Last minute
+        pass
+    
+    def halt(self): # Not implemented -> Last minute
+        pass
