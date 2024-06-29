58a9fb834e451fb9e37914a4895e0d25d0b3f692
Jevi Waugh <codeprogramming99@gmail.com>
2024-06-29 20:17:10 +1000

    multiprocessing

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from droid import Droid
 from object_detection import detect_purple_obstacle
 import multiprocessing
 from ipcqueue import posixmq
-from MotorAPI import MotorAPI_Reciever
+from MotorAPI import MotorAPI
 from VisionAPI import close_unlink_queue
-from VisionAPI import VisionAPI_Sender
+from VisionAPI import VisionAPI
 
 
 def main():
@@ -17,11 +17,12 @@ def main():
         # Motor to Vision
         M2V = posixmq.Queue('/DRC-Motor-Vision-data', maxmsgsize=1024)
         
-        # Create the sender and receiver processes
-        VISION_API_PROCESS = multiprocessing.Process(target=VisionAPI_Sender)
-        CONTROL_API_PROCESS = multiprocessing.Process(target=MotorAPI_Reciever)
+        # create processes and have both queues as arguments
+        VISION_API_PROCESS = multiprocessing.Process(target=VisionAPI, args=(V2M,M2V))
+        CONTROL_API_PROCESS = multiprocessing.Process(target=MotorAPI, args=(M2V,V2M))
+    
 
-        # Start the processes
+        # Start both processes
         VISION_API_PROCESS.start()
         CONTROL_API_PROCESS.start()
 
@@ -29,27 +30,11 @@ def main():
         VISION_API_PROCESS.join()
         CONTROL_API_PROCESS.join()
         
-        V2M.close_unlink_queue()
-        M2V.close_unlink_queue()
+        # V2M.close_unlink_queue()
+        # M2V.close_unlink_queue()
+        
         print("Communication between processes has ended.")
-        
-   
-        # 1. invoke thread
-        # thread1 = cx, cy = droid.detect_track()
-        # deviation = droid.distance_to_turn(droid.frame.shape[1], cx=droid.center_x)
-        
-        # 2. invoke thread 
-        # thread2 = droid.directional_capabilities(deviation)
-        
-        # 3. invoke thread 
-        # thread3 = detect_purple_obstacle(droid)
-        
-        
-        # 4. invoke thread here for arrow detection
-        # thread4 = detect_arrow(droid)
-        
-        
-        
+    
         # droid.close_camera()
         counter += 1
         
