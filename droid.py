e02b82995a2ee1ae2aaa5dac86f3dca1f14c76f3
Jevi Waugh <codeprogramming99@gmail.com>
2024-06-29 20:16:31 +1000

    object testing

--- droid.py
+++ droid.py
@@ -70,7 +70,12 @@ class Droid():
     # ^^^^^^^
     # |||||||
     
-    
+    def distance_to_turn(self, frame_width, cx, arrow=None) -> int:
+        """This function calculates the difference between the track and the center of the camera.
+        Thus returns the difference for how much distance there is to turn for the steering function"""
+        frame_center = frame_width // 2
+        derivative = frame_center - cx
+        return derivative
         
     def test_obstacle_detection(self, i, text, image): #Testing function
         """This function is only created to test the obstacle detection algorithm not to be deployed"""
@@ -117,62 +122,7 @@ class Droid():
         # experimenting adjustment for the range of yellow
         # self.yellow_lower = np.array([light_yellow_hsv_img[0] - 10, 100, 100])
         # self.yellow_upper = np.array([light_yellow_hsv_img[0] + 10, 255, 255])
-    
-    def directional_capabilities(self, derivative: int, degree=60) -> None: # Almost Done
-        """Not now, but i need to adjust the fps if the derivative is too high"""
-        """This basically makes the droid go around a curvy track"""
-        
-        distance = self.estimate_object_distance()
-        # use this distance to fine tune the avoid_obstacle function
-        avoid = True if self.obstacle and self.obstacle_area[0] * self.obstacle_area[1] > self.obstacle_area_thresh else False
-        if avoid:
-                self.avoid_obstacle()
-        if derivative < 0:
-            # we'll fine tune the degree here when we test it depending on how big the derivative is
-            self.turn_right(degree)
-            
-        elif derivative > 0 :
-            self.turn_left(degree)
-        else:
-            self.move_foward()
-            
-    def avoid_obstacle(self): # Almost Done
-        # get readings from ultrasonic sensor.
-        # seconds is from the data of the sensor
-        self.recalibration_function()
-        #only issue with this is that the centroid could be on the far siude of the other edge miscalculating the distance
-        
-        # IMPLEMENT BELOW
-        
-        
-        # self.purple_centroid[0]
-        degree = self.map_boundboxdim2_framedim() 
-        
-        seconds = 0 # imaging getting this from the sensor thread.
-        if (self.purple_centroid[0] - self.center_x) > 0:
-            # and there is sufficient distance between the obstacle and the left tape
-            # then go around the LEFT side of the object
-            # degree is based off the distance
-            self.halt()
-            self.turn_left(degree)
-            self.move_foward(seconds)
-            self.turn_right(degree)
-            self.move_foward(seconds)
-        elif (self.purple_centroid[0] - self.center_x) < 0:
-            # then go around the RIGHT side of the object
-            self.halt()
-            self.turn_right(degree)
-            self.move_foward(seconds)
-            self.turn_left(degree)
-            self.move_foward(seconds)
-    
-    def distance_to_turn(self, frame_width, cx, arrow=None) -> int:
-        """This function calculates the difference between the track and the center of the camera.
-        Thus returns the difference for how much distance there is to turn for the steering function"""
-        frame_center = frame_width // 2
-        derivative = frame_center - cx
-        return derivative
-            
+       
     def open_camera(self) -> bool:
         # Create a VideoCapture object to capture frames from the webcam
         if not self.camera.isOpened():
