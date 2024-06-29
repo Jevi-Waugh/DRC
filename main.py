43024624e00a830ebde910937f2e485d7d4b836a
Jevi Waugh <codeprogramming99@gmail.com>
2024-06-29 19:29:55 +1000

    multiprocessing structure

--- main.py
+++ main.py
@@ -2,19 +2,41 @@ import cv2 as cv
 import numpy as np
 from droid import Droid
 from object_detection import detect_purple_obstacle
+import multiprocessing
+from ipcqueue import posixmq
+from MotorAPI import MotorAPI_Reciever
+from VisionAPI import close_unlink_queue
+from VisionAPI import VisionAPI_Sender
+
 
 def main():
     counter = 0
     while counter != 1:
-        droid = Droid(camera_index = 0)
-        # keep updating droid status
+        # Vision to Motor
+        V2M = posixmq.Queue('/DRC-Vision-Motor-data', maxmsgsize=1024)
+        # Motor to Vision
+        M2V = posixmq.Queue('/DRC-Motor-Vision-data', maxmsgsize=1024)
         
-        droid.rbg_2_hsv()()
-        # what if cx was only calculated with one tape -> 1 bug here
-        # create a recalibration function to find both tapes i guess.
-        deviation = droid.distance_to_turn(droid.frame.shape[1], cx=droid.center_x)
+        # Create the sender and receiver processes
+        VISION_API_PROCESS = multiprocessing.Process(target=VisionAPI_Sender)
+        CONTROL_API_PROCESS = multiprocessing.Process(target=MotorAPI_Reciever)
+
+        # Start the processes
+        VISION_API_PROCESS.start()
+        CONTROL_API_PROCESS.start()
+
+        # Wait for the processes to finish
+        VISION_API_PROCESS.join()
+        CONTROL_API_PROCESS.join()
+        
+        V2M.close_unlink_queue()
+        M2V.close_unlink_queue()
+        print("Communication between processes has ended.")
+        
+   
         # 1. invoke thread
         # thread1 = cx, cy = droid.detect_track()
+        # deviation = droid.distance_to_turn(droid.frame.shape[1], cx=droid.center_x)
         
         # 2. invoke thread 
         # thread2 = droid.directional_capabilities(deviation)
@@ -33,3 +55,5 @@ def main():
         
 if __name__ == "__main__":
     main()
+    
+    
