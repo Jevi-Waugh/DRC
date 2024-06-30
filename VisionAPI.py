c61fb2ff1d94c97748ba8ff704916dd387ab5fcf
Jevi Waugh <codeprogramming99@gmail.com>
2024-06-30 21:48:02 +1000

    fixed pipeline bugs

--- VisionAPI.py
+++ VisionAPI.py
@@ -1,9 +1,9 @@
 import time
-from ipcqueue import posixmq
+# from ipcqueue import posixmq
 from droid import Droid
 from object_detection import detect_purple_obstacle
 
-def VisionAPI(send_q, recieve_q):
+def VisionAPI(send_q=None, recieve_q=None):
     """This will have the vision Process and will send input and recive output
     Not sure if this will be the final structure for the spatial analysis and activity
     but Multithreading is strongly on the table"""
@@ -13,8 +13,10 @@ def VisionAPI(send_q, recieve_q):
     droid = Droid(camera_index = 0)
     droid.rbg_2_hsv()
     cx, cy = droid.detect_track()
-    deviation = droid.distance_to_turn(droid.frame.shape[1], cx=droid.center_x)
+    # droid.center_x = 320
+    deviation = droid.distance_to_turn(droid.frame.shape[1], cX=droid.center_x)
     obstacle = detect_purple_obstacle(droid)
+    print(f"deviaton={deviation}, obstacle={obstacle}")
     # detect arrow here
     
     # setting up queue
@@ -27,3 +29,6 @@ def VisionAPI(send_q, recieve_q):
 def close_unlink_queue(queue):
     queue.close()
     queue.unlink()
+    
+
+VisionAPI()
