065e483c1101c10685c23dc7c2bd4245f9d580f1
Jevi Waugh <codeprogramming99@gmail.com>
2024-07-05 16:03:12 +1000

    "Modified harris_corner_detection function to take an additional binary_mask parameter and updated detect_7_corners function with a comment."

--- arrow_detect.py
+++ arrow_detect.py
@@ -1,9 +1,9 @@
-def harris_corner_detection(self):
+def harris_corner_detection(self, binary_mask):
     pass
 
 
 def detect_7_corners(self):
     # get the Contour
     # use harris corner detection on the contour mask or photo
-    
+    # but find a way to accurately expect the arrow
     pass
