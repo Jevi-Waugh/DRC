20139788dcfdff61e00aa32561a8f915feaa922d
Jevi Waugh <codeprogramming99@gmail.com>
2024-07-06 21:48:23 +1000

    main notion for arrow detection system

---
+++ arrow_detect.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+def harris_corner_detection(self, binary_mask) -> int:
+    frame = cv.cornerHarris()
+    pass
+    # return length of corners
+
+
+def detect_7_corners(self):
+    # get the Contour
+    # use harris corner detection on the contour mask or photo
+    # but find a way to accurately expect the arrow
+    pass
+
+
+def arrow_detect(self):
+    # process frame and use a bianry mask such as black and white
+    
+    # get the contour of that mask
+    
+    
+    # use harris corner detection on the contour mask
+    
+    
+    # make sure that the center of the frame is in line with the center of the arrow
+    # if not adjust
+    
+    
+    # if there are pixel values to the left of the frame then it is a left arrow
+    
+    # else a right arrow
