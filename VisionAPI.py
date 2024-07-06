764bb2f409fbe8148a4f3cadf85224db7867bca3
Jevi Waugh <codeprogramming99@gmail.com>
2024-07-06 21:47:04 +1000

    vision files

--- VisionAPI.py
+++ VisionAPI.py
@@ -13,24 +13,21 @@ def VisionAPI(send_q=None, recieve_q=None):
     # this might be done in main if process can handle enough byte for the Droid object
     droid = Droid(camera_index = 0, droid_status=True)
     droid.deploy_rgb_2_hsv()
-    # below is to repeat
-    # Detect green line first.
-   
+
     # if green line is detected, then GO!
     cx, cy = droid.detect_track()
     droid.green_line = True if not droid.green_contours else False
-    # droid.center_x = 320
     deviation = droid.distance_to_turn(droid.frame.shape[1], cX=droid.center_x)
     obstacle = detect_purple_obstacle(droid)
     print(f"deviaton={deviation}, obstacle={obstacle}")
     print(droid.__repr__())
             
             
-            # Finish line
-             
-            
-            # By the time this statement is reacged the droid would have probably gone off the 
-            # green start line unable to pick it uop again restting its value therefore this is feasable
+    # Finish line
+        
+    
+    # By the time this statement is reacged the droid would have probably gone off the 
+    # green start line unable to pick it uop again restting its value therefore this is feasable
     
     # Need to put data
     # detect arrow here
