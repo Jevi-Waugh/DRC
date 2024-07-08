3ff1b61f6b001b7c5ae63c6e321b055f2a5ef050
Jevi Waugh <codeprogramming99@gmail.com>
2024-07-08 22:00:16 +1000

    updated arrow_detect

--- arrow_detect.py
+++ arrow_detect.py
@@ -9,16 +9,13 @@ def harris_corner_detection(self, binary_mask) -> list:
     # return corners
 
 
-def arrow_detect(self, vision_sys: Vision, frame):
+def arrow_detect(self, vision_sys: Vision, frame) -> bool:
     # process frame and use a bianry mask such as black and white
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     processed_frame = cv.GaussianBlur(gray_frame, (5, 5), 0)
- 
-    
-    
     # get the contour of that mask
     # BUG
-    # Issue is that i might feed it white oine to detect the arrow
+    # Issue is that i might feed it white sheet to detect the arrow
     # but the contour itself will be the sheet and not the arrow.
     # i guess once i find 4 corners of the white sheet then get the frame inside that and get the contour again 
     # then expect 7 corners.
@@ -26,8 +23,19 @@ def arrow_detect(self, vision_sys: Vision, frame):
     corners = harris_corner_detection(contour)
     
     # use harris corner detection on the contour mask
-    
-    
+    if len(corners) == 7:
+        vision_sys.arrow = True
+        # center the frame.
+        # frame[y, x]
+        # frame[start_row:end_row, start_col:end_col]
+        height, width, _ = frame.shape
+        left_frame = frame[:, :width]
+        # right_frame = frame[:, width:]
+        contour_lf = vision_sys.find_contours(left_frame)
+        # contour_rf = vision_sys.find_contours(right_frame)
+        # computationally expensive but need to check if we are correct.
+        vision_sys.arrow_dir = "left" if len(contour_lf) == 5 else "right"
+            
     # make sure that the center of the frame is in line with the center of the arrow
     # if not adjust
     
@@ -35,3 +43,4 @@ def arrow_detect(self, vision_sys: Vision, frame):
     # if there are pixel values to the left of the frame then it is a left arrow
     
     # else a right arrow
+    return vision_sys.arrow
